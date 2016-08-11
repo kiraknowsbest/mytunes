07a381cfb7b87f8fff9bb6fb1c2556ac277f12f3
kiraknowsbest <kinaharo@gmail.com>
2016-08-10 23:16:29 -0700

    Pass tests for Library View

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,10 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
 
+  initialize: function () {
+    this.url = 'https://api.parse.com/1/classes/songs/';
+    this.fetch();
+  }
 });
