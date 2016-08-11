cba983c39c64acb168ee7580cc52c60f7f375522
kiraknowsbest <kinaharo@gmail.com>
2016-08-10 20:47:44 -0700

    Pass tests for SongQueue.js

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
-  }
+    this.on('add', function(e) {
+      if ( e.collection.length === 1 ) {
+        this.playFirst();
+      }
+    });
 
+    this.on( 'ended', function(e) {
+      this.remove(this.at(0));
+      if ( this.at(0) ) {
+        this.playFirst();
+      }
+    });
+
+    this.on( 'dequeue', function(e) {
+      this.remove();
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
+  }
 });
